15536e44c48f26d2a0aa0abb4df899b86a608651
Jack (Jacques) Faulkner <hex@hexdev.xyz>
2020-12-14 19:58:12 +0000

    lol installer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const chalk = require('chalk');
+const {execSync} = require('child_process');
 const request = require('request');
 const progress = require('request-progress');
 const prompt = require('prompt-sync')();
@@ -13,8 +14,8 @@ function error(msg, exitcode) {
 }
 
 console.log(`Welcome to the ${chalk.italic('unofficial')} ${chalk.bold(chalk.cyan('KeyCraft'))} Modpack Installer.\nThis is for lazy cunts who don't know how to install mods.`);
-console.log(`${chalk.underline('Few things before we get started:')}\n${chalk.bold('- This will delete ALL mods in the mods folder. If you\'re using this installer it probably means you don\'t have anything in your mods folder.')}\n- If you have any issues, please DM HexDev#0001 on Discord, or open up an issue on the Github.\n- If you\'d like to suggest a mod, open up an issue on the Github.\n- This is a bodged installer. I cannot guarantee this will work. By using this installer you agree that I am not liable for any damages caused, and i provide no warranty.`);
-const yn = prompt('[Y/n] ');
+console.log(`${chalk.underline('Few things before we get started:')}\n${chalk.bold('- This will delete ALL mods in the mods folder. If you\'re using this installer it probably means you don\'t have anything in your mods folder anyways.')}\n- If you have any issues, please DM HexDev#0001 on Discord, or open up an issue on the Github.\n- If you\'d like to suggest a mod, open up an issue on the Github.\n- This is a bodged installer. I cannot guarantee this will work. By using this installer you agree that I am not liable for any damages caused, and i provide no warranty.`);
+let yn = prompt('[Y/n] ');
 
 if (yn.toLowerCase().includes('n')) {
     console.log('Cancelled');
@@ -22,34 +23,44 @@ if (yn.toLowerCase().includes('n')) {
     process.exit(0);
 }
 
+if (!fs.existsSync('./mods.zip')) {
+}
+
+console.clear();
+console.log('Would you like to install Forge 1.7.10?');
+yn = prompt('[y/N] ');
+
+if (yn.toLowerCase().includes('y')) {
+    console.log('Starting up Forge Installer.\nIn order to install: Mod System Installer > Install Client > Link to your .minecraft folder (should be already set) > Ok\nOnce done, simply close the pop-up and the installer will continue on.');
+    execSync('java -jar forge.jar');
+    console.log('Forge Installation complete.');
+}
+
 if (!fs.existsSync(process.env.APPDATA + '/.minecraft/mods/')) {
-    error('Your /mods directory is non existent. Please install Forge', 0);
+    error('Your /mods directory is non existent. Forge didn\'t install correctly. Please re-run the installer', 0);
 }
 
 console.log('Deleting mods folder...');
-fs.rmdirSync(process.env.APPDATA + '/.minecraft/mods', {recursive:true});
-fs.mkdirSync(process.env.APPDATA + '/.minecraft/mods');
+fs.removeSync(`${process.env.APPDATA}\\.minecraft\\mods`, {recursive:true});
+fs.mkdirSync(`${process.env.APPDATA}/.minecraft/mods`);
 
-
-let aaa = request.get('https://gist.githubusercontent.com/HexDevv/4fe538f9189e6150121f32949b7179dc/raw/38740fe419184a403c400691b1fa538d18b0f381/KeyCraft-Config.json')
-aaa = aaa.json();
-const downloadURL = aaa.downloadURL;
-
-const modFILE = fs.createWriteStream('./mods.zip');
-const download = progress(request.get(downloadURL));
-
-download.on('progress', function (state) {
-    let downloadPercentage = '';
-    for (let i=0;i<20;i++) {
-        if (i<state.percent) downloadPercentage = downloadPercentage + '#';
-        else downloadPercentage = downloadPercentage + '-';
-    }
-    console.log(`[${downloadPercentage}] ${state/1000000}MB/s`);
-});
-
-download.on('response', (res) => {
+request.get('https://pastebin.com/raw/qQzFqYpd', (err, res, body) => {
     if (res.statusCode != 200) error(`HTTP Error Status is ${res.statusCode}`, 0);
-    res.pipe(modFILE);
+    const downloadURL = JSON.parse(body).downloadURL;
+    
+    const modFILE = fs.createWriteStream('./mods.zip');
+    progress(request.get(downloadURL))
+    .on('progress', function (state) {
+        console.log(state);
+        let downloadPercentage = '';
+        for (let i=0;i<20;i++) {
+            if (i<state.percent) downloadPercentage = downloadPercentage + '#';
+            else downloadPercentage = downloadPercentage + '-';
+        }
+        console.log(`[${downloadPercentage}] ${(state.speed/1000000).toFixed(2)}MB/s`);
+    })
+    .on('response', (res) => {
+        res.pipe(modFILE);
+    })
+    .on('end', () => fs.unlink('./mods.zip', () => {}));
 });
-
-download.on('finish', () => fs.unlink('./mods.zip', () => {}));
